64496e4e52ebceba672290222e028a29ca58f21d
JulienGAMartin <julien.martin@uottawa.ca>
2024-02-21 14:37:49 -0500

    moving r function

--- _extensions/juliengamartin/themes_board.r
+++ _extensions/juliengamartin/themes_board.r
@@ -5,7 +5,6 @@ showtext_auto()
 knitr::opts_chunk$set(dev.args = list(bg = "transparent"))
 
 # theme for chalkboard
-# theme for chalkboard
 theme_blackboard <- function(...) {
   cowplot::theme_cowplot() %+replace%
     theme(
@@ -26,7 +25,7 @@ theme_blackboard <- function(...) {
     )
 }
 
-check_model_w <- function(mod){
+check_model_bb <- function(mod){
   pp <- check_model(mod)
   p <- plot(pp, return_list = TRUE)
   out <- p & (theme_blackboard() +
@@ -39,4 +38,36 @@ check_model_w <- function(mod){
   out
 }
 
-theme_set(theme_blackboard())
+# theme for whiteboard
+theme_whiteboard <- function(...) {
+  cowplot::theme_cowplot() %+replace%
+    theme(
+      axis.ticks = element_line(colour = "black", size = 0.25),
+      text = element_text(colour = "black"),
+      axis.text = element_text(colour = "black", family = "Pangolin", size = 16),
+      axis.title = element_text(colour = "black", family = "Pangolin", size = 24),
+      axis.line = element_line(colour = "black", linewidth = 1),
+      panel.background = element_rect(colour = NA, fill = "transparent"),
+      # plot.background = element_rect(colour = "white", fill = "transparent"),
+      legend.position = "bottom",
+      legend.title = element_blank(),
+      panel.grid.minor = element_blank(),
+      # panel.grid.major.x = element_line(colour = "white", size = 0.25),
+      # panel.grid.major.y = element_line(colour = "white", size = 0.25),
+      legend.text = element_text(size = 24),
+      ...
+    )
+}
+
+check_model_wb <- function(mod) {
+  pp <- check_model(mod)
+  p <- plot(pp, return_list = TRUE)
+  out <- p & (theme_whiteboard() +
+    theme(
+      axis.text = element_text(size = 10),
+      axis.title = element_text(size = 12),
+      legend.text = element_text(size = 10)
+    )
+  )
+  out
+}
